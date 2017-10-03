b896ba725f745e8c271bb642bd895af1ccf009ac
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-02 20:40:36 -0400

    first

---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SoundTest : MonoBehaviour {
+
+    public string url = "Test";
+    public AudioSource audio;
+
+    private IEnumerator Start() {
+        audio.clip = Resources.Load(url) as AudioClip;
+        audio.Play();
+
+        yield return null;
+    }
+
+}
